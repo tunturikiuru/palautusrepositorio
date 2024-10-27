28217205e581ddd02a8238403242c6efd93bc939
Anna Salminiitty <koivu05@gmail.com>
2024-10-27 20:38:23 +0200

    tehtava12

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,25 +1,17 @@
-import requests
-from player import Player
+import player
+from player_reader import PlayerReader
+from player_stats import PlayerStats
 
 def main():
+
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
-    response = requests.get(url).json()
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
-    #print("JSON-muotoinen vastaus:")
-    #print(response)
-
-    players = []
-
-    for player_dict in response:
-        if player_dict['nationality'] == 'FIN':
-            player = Player(player_dict)
-            players.append(player)
-
-    sorted_players = sorted(players, reverse = True, key= lambda player: player.goals + player.assists)
-
-    print("Players from FIN\n")
-    for player in sorted_players:
+    for player in players:
         print(player)
+
 
 if __name__ == "__main__":
     main()
---
+++ viikko2/nhl-reader/src/player_stats.py
@@ -0,0 +1,10 @@
+import player_reader
+
+class PlayerStats:
+    def __init__(self, reader):
+        self.players = reader.get_players()
+
+    def top_scorers_by_nationality(self, nationality):
+        player_list = filter(lambda player: player.nationality == nationality, self.players)
+        sorted_players = sorted(player_list, reverse = True, key= lambda player: player.goals + player.assists)
+        return sorted_players
