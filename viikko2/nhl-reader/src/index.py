c1e98c7c2d4f78d692f57afb03a35283322febbb
Anna Salminiitty <koivu05@gmail.com>
2024-10-28 21:34:27 +0200

    tehtava13

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,16 +1,52 @@
 import player
+from rich import print
+from rich.table import Table
 from player_reader import PlayerReader
 from player_stats import PlayerStats
 
 def main():
-
-    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
+    season = get_season()
+    if season == "":
+        return
+    url = f'https://studies.cs.helsinki.fi/nhlstats/{season}/players'
     reader = PlayerReader(url)
     stats = PlayerStats(reader)
-    players = stats.top_scorers_by_nationality("FIN")
+    nationalities = stats.nationalities()
+    print_stats(stats, nationalities, season)
+
+
+def get_season():
+    print("[italic]NHL statistics by nationality[/italic]\n")
+    while True:
+        print("Select season [bold magenta][2018-19/2019-20/2020-21/2021-22/2022-23/2023-24/2024-25][/bold magenta]:")
+        season = input()
+        if season == "":
+            return ""
+        if season == "2018-19" or "2019-20" or "2020-21" or "2021-22" or "2022-23" or "2023-24" or "2024-25":
+            return season
+
+def print_stats(stats, nationalities, season):
+    while True:
+        print(f'Select nationality [bold magenta][{nationalities}][/bold magenta]:')
+        nationality = input()
+        if nationality == "":
+            break
+        players = stats.top_scorers_by_nationality(nationality)
+        print_table(players, nationality, season)
+
+
+def print_table(players, nationality, season):
+    table = Table(title=f"Top scorers of {nationality} season {season}")
+    table.add_column("name", justify="left", style="cyan")
+    table.add_column("team", justify="left", style="magenta")
+    table.add_column("goals", justify="right", style="green")
+    table.add_column("assists", justify="right", style="green")
+    table.add_column("points", justify="right", style="green")
 
     for player in players:
-        print(player)
+        table.add_row(player.name, player.team, str(player.goals), str(player.assists), str(player.goals + player.assists))
+    
+    print(table)
 
 
 if __name__ == "__main__":
--- viikko2/nhl-reader/src/player_stats.py
+++ viikko2/nhl-reader/src/player_stats.py
@@ -4,6 +4,11 @@ class PlayerStats:
     def __init__(self, reader):
         self.players = reader.get_players()
 
+    def nationalities(self):
+        nationalities = {player.nationality for player in self.players}
+        return "/".join(nationalities)
+
+
     def top_scorers_by_nationality(self, nationality):
         player_list = filter(lambda player: player.nationality == nationality, self.players)
         sorted_players = sorted(player_list, reverse = True, key= lambda player: player.goals + player.assists)
