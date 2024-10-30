e9ab0b4170739f4c94c7318fc4ccd5b50f931528
Anna Salminiitty <koivu05@gmail.com>
2024-10-30 18:47:28 +0200

    teht8 tulo.py

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -3,6 +3,7 @@
 from logger import logger
 from summa import summa
 from erotus import erotus
+from tulo import tulo
 
 logger("aloitetaan ohjelma")
 
@@ -10,6 +11,7 @@ x = int(input("luku 1: "))
 y = int(input("luku 2: "))
 print(f"Lukujen {x} ja {y} summa on {summa(x, y)}")  # valittu muutos
 print(f"Lukujen {x} ja {y} erotus on {erotus(x, y)}")  # valitty muutos
+print(f"{x} * {y} = {tulo(x, y)}") 
 
 logger("lopetetaan")
 print("goodbye!")
