098b5c205091a9f2530637fa67b954a15d026958
Anna Salminiitty <koivu05@gmail.com>
2025-10-26 16:03:36 +0200

    1.15

---
+++ viikko1/nhl-statistics/src/index.py
@@ -0,0 +1,24 @@
+from player_reader import PlayerReader
+from statistics_service import StatisticsService
+
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players.txt"
+    reader = PlayerReader(url)
+    stats = StatisticsService(reader)
+    philadelphia_flyers_players = stats.team("PHI")
+    top_scorers = stats.top(10)
+
+    print("Philadelphia Flyers:")
+    for player in philadelphia_flyers_players:
+        print(player)
+
+    print()  
+
+    print("Top point getters:")
+    for player in top_scorers:
+        print(player)
+
+
+if __name__ == "__main__":
+    main()
